a995b2b277195dd6971a592b478104dd5af75395
Shravan Papanaidu <shravanpn7@gmail.com>
2014-10-04 13:20:14 -0700

    Catalog Ok

---
+++ WebContent/assets/js/product.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+    $('#selectMenuCategory').click (function (){
+        var category = $(this).attr("value");
+        //alert (" "+category);
+        $.get('getSubCategory', { category:category }, function (data){
+                $('#selectMenuSubCategory').html(data);
+        })
+    });
+});
+
